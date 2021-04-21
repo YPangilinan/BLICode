cd23ac3470afc7be16af777cd78c0fdc638e2dfb
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2021-04-20 21:52:59 -0700

    adding new unit test

--- merge.test.js
+++ merge.test.js
@@ -1,4 +1,4 @@
-//attempt at unit testing. Novice user
+//attempt at unit testing. Novice user to Jest
 
 const { expect } = require('@jest/globals')
 const parseInput = require('./merge.js')
@@ -7,3 +7,8 @@ test('should merge overlapping intervals', () => {
     const runString = "1,2-5,6,10-12,11-15"
     expect(parseInput(runString).toEqual[ [ 1, 1 ], [ 2, 5 ], [ 6, 6 ], [ 10, 15 ] ])
 });
+
+test('Value Error if string is null'), () =>{
+    expect(parseInput(null)).toBeNull();
+}
+
